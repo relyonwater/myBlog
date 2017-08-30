20436e47b2658be1e6d4be959ee00fda8e73c351
yuhuiping <fish@yuhuipingdeMacBook-Pro.local>
2017-08-30 16:19:07 +0800

    new feature

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -20,6 +20,6 @@ from article import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^$', views.home, name= 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 ]
